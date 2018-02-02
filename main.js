8cb6462b17c5e71dbb7da9cc6431ad92ab38e844
BruceChen034020 <2017greatplatform@gmail.com>
2018-02-03 01:10:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+/*
+感謝您觀看這份程式碼
+作品名稱: 取地名
+作者: 陳光穎 Bruce Chen
+聯絡方式
+Facebook連結: https://www.facebook.com/bruce.chen.372
+    LINE ID: brucechen0
+最後修改日期: 2017/7/?
+    版本: N/A
+發表於: https://brucechen034020.github.io/
+    程式碼尺度
+N/A
+作者註解:
+    1. 如本網頁有 bug 請用 Facebook(Messenger) 通知 Bruce Chen，謝謝。
+  2. 如有任何建議，請用 Facebook(Messenger) 通知 Bruce Chen，謝謝。
+*/
 
 /* Global variables */
-var grid;
-var cols;
-var rows;
-var w = 20;
+var grid; // Grid (2D Cell array)
+var cols; // number of columns (int)
+var rows; // number of rows (int)
+var w = 20; // length of each cell (int)
 
-var totalBees = 30;
-var beeRatio = 0;
+var totalBees = 30; // number of mines (int)
+var beeRatio = 0; // ratio of mines (int)
 
-var useRatio = false;
+var useRatio = false; // true: There are totalBees mines. false: There are approximately cols*rows*beeRatio mines
 
+var Sweeper; // Sweeper button (Button)
+var Marker; // Marker button (Marker)
+var marking; // User is now using marker rather than sweeper (bool)
+var ActivatedMineCount; // (p)
 /* p5 functions */
 function setup() {
   createCanvas(401, 401);
@@ -48,6 +68,23 @@ function setup() {
     }
   }
 
+  /* Set other elements */
+  createP('');
+
+  Sweeper = document.createElement("button");
+  document.body.appendChild(Sweeper);
+  Sweeper.innerHTML = "踩下去 Sweeper";
+  Sweeper.addEventListener("click", Sweeper_Clicked);
+
+  Marker = document.createElement("button");
+  document.body.appendChild(Marker);
+  Marker.innerHTML = "標記地雷 Marker";
+  Marker.addEventListener("click", Marker_Clicked);
+
+  ActivatedMineCount = document.createElement("p");
+  document.body.appendChild(ActivatedMineCount);
+  ActivatedMineCount.innerHTML = "7 mines are activated.";
+  ActivatedMineCount.value = "7 mines are activated."
 }
 
 function mousePressed() {
@@ -88,4 +125,11 @@ function gameOver() {
       grid[i][j].revealed = true;
     }
   }
+}
+
+function Sweeper_Clicked(){
+  console.log("Sweeper");
+}
+function Marker_Clicked(){
+  console.log("Marker");
 }
